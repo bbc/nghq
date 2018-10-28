7a9dd878a8ddd9b6fdc82cb06c7b820e9a29664d
Rafee <mij.rafee.1344@gmail.com>
2018-10-28 19:34:19 -0400

    Added depth to git submodules and made build script as initializer. Build directory within git repository

--- build_nghq.sh
+++ build_nghq.sh
@@ -1,8 +1,11 @@
-#echo $PWD
+
+
+git submodule init
+git submodule update
 
 cd openssl
 # For Linux
-./config enable-tls1_3 --prefix=$PWD/../../build/openssl
+./config enable-tls1_3 --prefix=$PWD/../build/openssl
 make -j$(nproc)
 make install_sw
 cd ..
@@ -12,12 +15,12 @@ autoreconf -i
 # For Mac users who have installed libev with MacPorts, append
 # ',-L/opt/local/lib' to LDFLAGS, and also pass
 # CPPFLAGS="-I/opt/local/include" to ./configure.
-./configure PKG_CONFIG_PATH=$PWD/../../build/openssl/lib/pkgconfig LDFLAGS="-Wl,-rpath,$PWD/../../build/openssl/lib" --prefix=$PWD/../../build/ngtcp2
+./configure PKG_CONFIG_PATH=$PWD/../build/openssl/lib/pkgconfig LDFLAGS="-Wl,-rpath,$PWD/../build/openssl/lib" --prefix=$PWD/../build/ngtcp2
 make -j$(nproc) check
 make install
 cd ..
 
 #cd nghq
 ./bootstrap
-./configure PKG_CONFIG_PATH=$PWD/../build/ngtcp2/lib/pkgconfig LDFLAGS="-Wl,-rpath,$PWD/../build/ngtcp2/lib" --prefix=$PWD/../build/nghq
+./configure PKG_CONFIG_PATH=$PWD/build/ngtcp2/lib/pkgconfig LDFLAGS="-Wl,-rpath,$PWD/build/ngtcp2/lib" --prefix=$PWD/build/nghq
 make install
