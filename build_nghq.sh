ec67c45af84fdd83a44372364c7b3bc30bec766a
Rafee <mij.rafee.1344@gmail.com>
2018-10-28 13:53:55 -0700

    Cleaned the build file for proper directory

--- build_nghq.sh
+++ build_nghq.sh
@@ -2,7 +2,7 @@
 
 cd openssl
 # For Linux
-./config enable-tls1_3 --prefix=$PWD/../build/openssl
+./config enable-tls1_3 --prefix=$PWD/../../build/openssl
 make -j$(nproc)
 make install_sw
 cd ..
@@ -12,12 +12,12 @@ autoreconf -i
 # For Mac users who have installed libev with MacPorts, append
 # ',-L/opt/local/lib' to LDFLAGS, and also pass
 # CPPFLAGS="-I/opt/local/include" to ./configure.
-./configure PKG_CONFIG_PATH=$PWD/../openssl/build/lib/pkgconfig LDFLAGS="-Wl,-rpath,$PWD/../openssl/build/lib" --prefix=$PWD/../build/ngtcp2
+./configure PKG_CONFIG_PATH=$PWD/../../build/openssl/lib/pkgconfig LDFLAGS="-Wl,-rpath,$PWD/../../build/openssl/lib" --prefix=$PWD/../../build/ngtcp2
 make -j$(nproc) check
 make install
 cd ..
 
 #cd nghq
 ./bootstrap
-./configure PKG_CONFIG_PATH=$PWD/../ngtcp2/build/lib/pkgconfig LDFLAGS="-Wl,-rpath,$PWD/../ngtcp2/build/lib" --prefix=$PWD/../build/nghq
+./configure PKG_CONFIG_PATH=$PWD/../build/ngtcp2/lib/pkgconfig LDFLAGS="-Wl,-rpath,$PWD/../build/ngtcp2/lib" --prefix=$PWD/../build/nghq
 make install
